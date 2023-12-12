05b012213ec37f77b2590c37c162ddae41cc20e6
kurt <kjmojado21@gmail.com>
2023-12-12 20:24:18 +0800

    routes lessons

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello-world',function (){
+    return "IM THE LARAVEL MASTER";
 });
+
+Route::get('/hello/user/{id}',function ($id) {
+    return "hello user $id ";
+});
+
